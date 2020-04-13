5984075910db5c8a2e3d1cba4156cd178b0b8948
sigma-us <kyleconley2@gmail.com>
2020-04-13 14:28:24 -0700

    alteration

--- LeetCode/contiguous-subarray.js
+++ LeetCode/contiguous-subarray.js
@@ -5,20 +5,18 @@
 var findMaxLength = function (nums) {
     let sum = 0;
     let max_len = 0;
-    let ending_index = -1;
     let hM = {};
     let n = nums.length;
 
     for (let i = 0; i < n; i++) {
         // Add current element to sum 
 
-        sum += nums[i] === 0 ? -1 : nums[i];
+        sum += nums[i] === 0 ? -1 : 1;
 
         // To handle sum=0 at last index 
         console.log(sum, sum === 0, max_len, ending_index)
         if (sum == 0) {
             max_len = i + 1;
-            ending_index = i;
         }
 
         // If this sum is seen before, then update max_len 
@@ -27,7 +25,6 @@ var findMaxLength = function (nums) {
         if (hM[sum + n] !== undefined) {
             if (max_len < i - hM[sum + n]) {
                 max_len = i - hM[sum + n];
-                ending_index = i;
             }
         }
         else hM[sum + n] = i;
