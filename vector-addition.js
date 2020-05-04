f95aed7e5e53b080eb4477d29f9ed153edfd29cf
sigma-us <kyleconley2@gmail.com>
2020-05-03 17:13:17 -0700

    linear addition

---
+++ vector-addition.js
@@ -0,0 +1,35 @@
+'use strict';
+
+// radius helper function
+function radius(degree) {
+    return Math.PI / 180 * degree;
+}
+
+function opposite(angle, mag) {
+    return Math.sin(radius(angle)) * mag;
+}
+
+function adjacent(angle, mag) {
+    return Math.cos(radius(angle)) * mag;
+}
+
+function convert(angle, mag) {
+    let a = adjacent(angle, mag);
+    let o = opposite(angle, mag);
+    console.log(a, o);
+
+    return [a, o]
+}
+
+function vectorAdd(v1, v2) {
+    let v3 = [v1[0]+v2[0], v1[1]+v2[1]];
+    console.log(v3);
+
+    return v3;
+}
+
+
+let v1 = convert(40, 5);
+let v2 = convert(100, 6);
+
+let v3 = vectorAdd(v1, v2);
